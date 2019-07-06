c5e068e7fef50b2fc7ea67f1be0c163607f5cbd3
zjj9788 <zjj9788@163.com>
2019-07-06 17:33:29 +0800

    laozhang

--- app/src/main/java/com/isoft/demo/airplane/MainFragment4.java
+++ app/src/main/java/com/isoft/demo/airplane/MainFragment4.java
@@ -6,24 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment4 extends Fragment {
-
-
-    public MainFragment4() {
-        // Required empty public constructor
-    }
-
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_fragment4, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_fragment4, container, false);
+        listView = (ListView) view.findViewById(R.id.personCenter);
+        int[] img = {R.mipmap.logo, R.mipmap.logo, R.mipmap.logo, R.mipmap.logo};
+        String[] label = {"aaaa", "bbbb", "cccc", "dddd"};
+        String[] label1 = {"aaaa", "bbbb", "cccc", "dddd"};
+        List data = new ArrayList();
+        for (int i = 0; i < img.length; i++) {
+            Map map = new HashMap();
+            map.put("title", label[i]);
+            map.put("title1", label[i]);
+            map.put("img", img[i]);
+            data.add(map);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),
+                data, R.layout.my_fragment_sub,
+                new String[]{"title", "title1", "img"}, new int[]{R.id.label, R.id.label1, R.id.img});
+        listView.setAdapter(simpleAdapter);
+        return view;
     }
 
 }
